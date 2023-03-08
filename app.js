371566ec7ce99f4884bf47c7f06ffdcea74b80a9
Voldemort0702 <tommy.870702@gmail.com>
2023-03-07 20:09:15 -0500

    change photo

--- app.js
+++ app.js
@@ -15,7 +15,7 @@
 })();
 
 
-  
+
 // const sections = document.querySelectorAll('.section');
 // const sectBtns = document.querySelectorAll('.controls');
 // const sectBtn = document.querySelectorAll('.control');
